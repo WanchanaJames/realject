e87ec5487591303b1d35c5bbaef3c3ab2b5ba71f
4RUN555 <68030316@kmitl.ac.th>
2025-09-06 22:33:12 +0700

    เริ่มมาหน่อยแล้วนิดๆ

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from config import engine
 
+import tables.users as user_table
+
+user_table.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.get("/")
-async def roof():
-    return "Hello World"
+#@app.get("/")
+#async def roof():
+#    return "Hello World"
