cd86dd9b09afcae9c107aa01ffbb71cb1209cb58
4RUN555 <68030316@kmitl.ac.th>
2025-09-07 13:23:11 +0700

    okหน่อย

---
+++ repository/users.py
@@ -0,0 +1,43 @@
+from typing import TypeVar, Generic, Optional
+from sqlalchemy.orm import Session
+
+from datetime import datetime,timedelta
+from jose import JWTError, jwt
+from config import SECRET_KEY, ALGORITHM
+
+from fastapi import Depends, Request, HTTPException
+from fastapi.security import HTTPBearer, HTTPBasicCredentials
+
+T = Typevar('T')
+
+# users
+
+class BaseRepo():
+    @staticmethod
+    def insert(db: Session, model: Generic[T]):
+        db.add(model)
+        db.commid()
+        db.refresh(model)
+
+class UsersRepo(BaseRepo):
+    @staticmethod
+    def find_by_username(db: Session, model: Generic[T], username: str):
+        return db.querry(model).filter(model.uesrname == username).first()
+
+#token
+class JWTRepo():
+    def generate_token(date: dict, expires_delta: Opion[timedelta] = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_detla
+        else:
+            expire = datetime.utcnow() + timedelta(minutes = 15)
+            to_encode.update({"exp": expire})
+            encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
+            return encode_jwt
+    def decode_token(token: str):
+        try:
+            decode_token = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
+            return decode_token if decode_token['expires'] >= datetime.time() else None
+        except:
+            return()
